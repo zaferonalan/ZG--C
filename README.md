d5026f1e61f9aec4e29081905196fc27cb7a7007
zaferonalan <zafergunay7@gmail.com>
2021-12-04 18:41:07 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Programlama
+ C# Çalışmalarım
---
+++ console-programlama/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.Write("İsmininizi Giriniz : ");
+            string name = Console.ReadLine();
+            Console.Write("Soyisimizi Giriniz : ");
+            string surname = Console.ReadLine();
+
+            Console.WriteLine("Hoşgeldiniz " + name + " " + surname);
+            Console.ReadKey();
+
+
+        }
+    }
+}
